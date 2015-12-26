fbbb3cbabc56f13a9ab60ed8e49008da9154cf81
zhs007 <sssxueren@gmail.com>
2015-12-26 16:30:31 +0800

    ver 0.6.1

---
+++ samples/dsl.js
@@ -0,0 +1,30 @@
+"use strict";
+
+var fs = require('fs');
+var process = require('process');
+var dslcore = require('../lib/index');
+
+let output = dslcore.compileFile('xszr.ds');
+var grammarok = dslcore.checkGrammar(output, function (isok, err) {
+    if (!isok) {
+        console.log('Error => ' + err);
+    }
+});
+
+if (!grammarok) {
+    console.log('Error => checkGrammar()');
+
+    process.exit(0);
+}
+
+output = dslcore.procInMessage(output);
+output = dslcore.reverseObj(output);
+
+console.log('dslang compile finished!');
+fs.writeFileSync('xszr.json', JSON.stringify(output), 'utf-8');
+
+dslcore.exportExcel('xszr', output, function (isok, err) {
+    if (!isok) {
+        console.log('Error => ' + err);
+    }
+});
